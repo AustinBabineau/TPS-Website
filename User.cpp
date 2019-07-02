deda36589df746ed0293aac7ce1aa941c77d02a3
GoopyDinoEggDrop <AustinBabineau@gmail.com>
2019-07-02 11:24:06 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -1,58 +1,110 @@
 //C++ Class for User
 
-Class User{
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class User{
+
 public:
-getUserID();
-getCity();
-getState();
-getZipcode();
-getAddress();
+
+int getUserID();
+
+string getCity();
+
+string getState();
+
+string getZipcode();
+
+string getAddress();
+
 void setUserID(int x);
+
 void setCity(string x);
+
 void setState(string x);
+
 void setZipcode(string x);
+
 void setAddress(string x);
+
 private:
+
 int userID;
+
 string city;
+
 string state;
+
 string zipcode;
+
 string address;
-}
+
+};
+
 //Method Definitions
+
 //Getters
+
 int User::getUserID(){
+
  return userID; 
+
 }
+
 string User::getCity(){
+
  return city; 
+
 }
+
 string User::getState(){
+
  return state; 
+
 }
+
 string User::getZipcode(){
+
  return zipcode; 
+
 }
+
 string User::getAddress(){
+
  return address; 
+
 }
+
 //Setters
+
 void User::setUserID(int x){
+
  userID = x; 
+
 }
+
 void User::setCity(string x){
+
  city = x; 
+
 }
+
 void User::setState(string x){
+
  state = x; 
+
 }
+
 void User::setZipcode(string x){
+
  zipcode = x; 
+
 }
+
 void User::setAddress(string x){
+
  address = x; 
-}
-User::User()
-{
- 
+
 }
