d7476d5673ecca8e0137e56798083051bd848a83
GoopyDinoEggDrop <AustinBabineau@gmail.com>
2019-07-02 10:28:03 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -2,10 +2,42 @@
 
 Class User{
 public:
-
+int getUserID(){
+ return userID; 
+}
+string getCity(){
+ return city; 
+}
+string getState(){
+ return state; 
+}
+string getZipcode(){
+ return zipcode; 
+}
+string getAddress(){
+ return address; 
+}
+  
+void setUserID(int x){
+ userID = x; 
+}
+void setCity(string x){
+ city = x; 
+}
+void setState(string x){
+ state = x; 
+}
+void setZipcode(string x){
+ zipcode = x; 
+}
+void setAddress(string x){
+ address = x; 
+}
 
 private:
-
-
-
+int userID;
+string city;
+string state;
+string zipcode;
+string address;
 }
