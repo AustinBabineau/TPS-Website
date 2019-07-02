76ca4be8d8b6078d9c1fcbfe7f9eaae6fbdbd4c8
GoopyDinoEggDrop <AustinBabineau@gmail.com>
2019-07-02 10:39:53 -0500

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -2,42 +2,57 @@
 
 Class User{
 public:
-int getUserID(){
- return userID; 
-}
-string getCity(){
- return city; 
-}
-string getState(){
- return state; 
-}
-string getZipcode(){
- return zipcode; 
-}
-string getAddress(){
- return address; 
-}
-  
-void setUserID(int x){
- userID = x; 
-}
-void setCity(string x){
- city = x; 
-}
-void setState(string x){
- state = x; 
-}
-void setZipcode(string x){
- zipcode = x; 
-}
-void setAddress(string x){
- address = x; 
-}
-
+getUserID();
+getCity();
+getState();
+getZipcode();
+getAddress();
+void setUserID(int x);
+void setCity(string x);
+void setState(string x);
+void setZipcode(string x);
+void setAddress(string x);
 private:
 int userID;
 string city;
 string state;
 string zipcode;
 string address;
+}
+//Method Definitions
+//Getters
+int User::getUserID(){
+ return userID; 
+}
+string User::getCity(){
+ return city; 
+}
+string User::getState(){
+ return state; 
+}
+string User::getZipcode(){
+ return zipcode; 
+}
+string User::getAddress(){
+ return address; 
+}
+//Setters
+void User::setUserID(int x){
+ userID = x; 
+}
+void User::setCity(string x){
+ city = x; 
+}
+void User::setState(string x){
+ state = x; 
+}
+void User::setZipcode(string x){
+ zipcode = x; 
+}
+void User::setAddress(string x){
+ address = x; 
+}
+User::User()
+{
+ 
 }
